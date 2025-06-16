66db7ae94d75fb097bb89b44e6808a8a67038bcb
Hira_Nawaz <nawazhira48@gmail.com>
2025-06-16 15:22:49 +0500

    Create bfs.cpp

---
+++ Graphs/bfs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void bfs(int start, vector<vector<int>> &adj, vector<bool> &visited) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+
+    while(!q.empty()) {
+        int node = q.front(); q.pop();
+        cout << node << " ";
+        for(int neighbor : adj[node]) {
+            if(!visited[neighbor]) {
+                q.push(neighbor);
+                visited[neighbor] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    int n = 5;
+    vector<vector<int>> adj(n);
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 4};
+    adj[3] = {1};
+    adj[4] = {2};
+    vector<bool> visited(n, false);
+    bfs(0, adj, visited);
+}
